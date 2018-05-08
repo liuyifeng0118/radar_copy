71ac1eb47cfc6359a3d168657e1d1007d17e8898
yifeng.liu <792357325@qq.com>
2018-05-08 13:32:55 +0800

    删除部分代码

--- src/test.java
+++ src/test.java
@@ -6,12 +6,5 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("cc");
-		System.out.println("dd");
-        System.out.println("aa");
-        System.out.println("bb");
-	    System.out.println("xx");
-	    System.out.println("ww");
-	    System.out.println("yy");
-	    System.out.println("zz");
     }
 }
