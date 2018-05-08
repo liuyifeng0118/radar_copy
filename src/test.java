b005f4b6e1fe26d7c6763f8e1ce47742fed1334d
yifeng.liu <792357325@qq.com>
2018-05-08 14:39:25 +0800

    Merge branch 'master' into release-1.0.1
    # Conflicts:
    #	src/test.java

--- src/test.java
+++ src/test.java
@@ -7,5 +7,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("cc");
         System.out.println("小米份");
+        System.out.println("aa");
+        System.out.println("bb");
     }
 }
